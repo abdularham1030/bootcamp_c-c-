66fdad99e88be10d6c6c2d8bbea75c95d789518c
abdularham1030 <98842127+abdularham1030@users.noreply.github.com>
2022-09-24 12:48:14 +0530

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,1 +1,27 @@
 
+class Solution
+{
+public:
+    bool isPalindrome(int x) 
+    {
+        int last = 0 ,count=x;
+        long int temp =0 ;
+        if(x < 0)
+            {
+                return false;
+            }
+        while(x)
+        {
+            last = x % 10 ;
+            temp = (temp + last)*10;
+            x = x/10;   
+        }
+        temp = temp / 10 ;
+        if(count == temp)
+        {
+            return true;
+        }
+        else 
+           return false;
+    }
+};
